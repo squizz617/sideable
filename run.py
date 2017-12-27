41cb6a542bdfa8ad976e741540fd1377fc55056f
Seulbae Kim <squizz617@gmail.com>
2017-12-27 21:24:25 +0900

    implementing cfg as dict & trying out trace isolation

--- run.py
+++ run.py
@@ -7,12 +7,13 @@ from the given ELF binary file.
 import os
 import subprocess
 import cPickle
+# from anytree import Node, RenderTree
 
 class BasicBlock():
     bid = None              # basic block's id
     start_ea = None         # starting address
     end_ea = None           # ending address
-    # pred_blocks = []        # list of predecessor blocks' id
+    pred_blocks = []        # list of predecessor blocks' id
     succ_blocks = []        # list of successor blocks' id
     addr_inst_map = {}      # map of address to instruction
     addr_inst_abs_map = {}  # map of address to abstract instruction
@@ -41,34 +42,85 @@ def analyze(target):
 
     return bb_list
 
+cfg1 = {}
+cfg2 = {}
+
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    if not graph.has_key(start):
+        return []
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
+
 
 def main():
     bb_list1 = analyze("date-822")
-    # bb_list2 = analyze("date-823")
+    bb_list2 = analyze("date-823")
 
     for bb1 in bb_list1:
-        print bb1.bid, bb1.succ_blocks, bb1.addr_inst_abs_map
+        # print bb1.bid, bb1.succ_blocks
+        # for addr in sorted(bb1.addr_inst_map):
+        #     print addr, bb1.addr_inst_map[addr]
+        cfg1[bb1.bid] = bb1.succ_blocks
+    #     # print bb1.pred_blocks, bb1.succ_blocks
+    #     if bb1.bid not in bbTree:
+    #         bbTree[bb1.bid] = Node(str(bb1.bid))
+    #     for succ in bb1.succ_blocks:
+    #         if succ not in bbTree:
+    #             bbTree[succ] = Node(str(succ), parent=bbTree[bb1.bid])
+    # print bbTree
+    # print RenderTree(bbTree[0])
+    entry = 0
+    exit = len(bb_list1) - 1
+
+    # target = 50
+    # print find_all_paths(cfg1, entry, target)
+    # print find_all_paths(cfg1, target, exit)
+
 
     # print len(bb_list1), "elements in list1"
     # print len(bb_list2), "elements in list2"
 
-    # match_cnt = 0
-    # match_list_bb1 = []
-    # for bb1 in bb_list1:
-    #     print bb1.addr_inst_map
-    #     match = 0
-    #     for bb2 in bb_list2:
-    #         if bb1.inst_hash == bb2.inst_hash:
-    #             print "match @", bb1.bid, bb2.bid
-    #             match = 1
-    #     if match:
-    #         match_list_bb1.append(bb1.bid)
-    #         match_cnt += 1
+    match_cnt = 0
+    match_list_bb1 = []
+    match_list_bb2 = []
+    for bb1 in bb_list1:
+        # print bb1.addr_inst_map
+        match = 0
+        for bb2 in bb_list2:
+            # if bb1.inst_hash == bb2.inst_hash:
+            if bb1.inst_abs_hash == bb2.inst_abs_hash:
+                print "match @", bb1.bid, bb2.bid
+                match_list_bb2.append(bb2.bid)
+                match = 1
+        if match:
+            match_list_bb1.append(bb1.bid)
+            match_cnt += 1
 
-    # print match_cnt, "matches"
-    # for bb1 in bb_list1:
-    #     if bb1.bid not in match_list_bb1:
-    #         print bb1.bid
+    print match_cnt, "matches"
+    for bb1 in bb_list1:
+        if bb1.bid not in match_list_bb1:
+            print bb1.bid
+            for addr in sorted(bb1.addr_inst_map):
+                print addr, bb1.addr_inst_map[addr]
+            target = bb1.bid
+            # print find_all_paths(cfg1, entry, target)
+            # print find_all_paths(cfg1, target, exit)
+
+    for bb2 in bb_list2:
+        if bb2.bid not in match_list_bb2:
+            print bb2.bid
+            for addr in sorted(bb2.addr_inst_map):
+                print addr, bb2.addr_inst_map[addr]
+
+
 
 if __name__ == "__main__":
     main()
